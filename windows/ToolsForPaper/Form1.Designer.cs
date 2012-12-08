c650c2d85937c710701deb7303add485cd69bfa0
Atsushi OHNO <atsushi-o@is.naist.jp>
2012-12-09 03:17:57 +0900

    Add windows client

---
+++ windows/ToolsForPaper/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ToolsForPaper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.singleCapMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.seqCapMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(964, 812);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSplitButton1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 790);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(964, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripSplitButton1
+            // 
+            this.toolStripSplitButton1.AutoToolTip = false;
+            this.toolStripSplitButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripSplitButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.singleCapMenuItem,
+            this.seqCapMenuItem});
+            this.toolStripSplitButton1.Image = global::ToolsForPaper.Properties.Resources.enable;
+            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(32, 20);
+            this.toolStripSplitButton1.Text = "toolStripSplitButton1";
+            this.toolStripSplitButton1.ToolTipText = "クリップボード監視中";
+            this.toolStripSplitButton1.ButtonClick += new System.EventHandler(this.toolStripSplitButton1_ButtonClick);
+            // 
+            // singleCapMenuItem
+            // 
+            this.singleCapMenuItem.Name = "singleCapMenuItem";
+            this.singleCapMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.singleCapMenuItem.Text = "シングルキャプチャ";
+            this.singleCapMenuItem.Click += new System.EventHandler(this.singleCapMenuItem_Click);
+            // 
+            // seqCapMenuItem
+            // 
+            this.seqCapMenuItem.Checked = true;
+            this.seqCapMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.seqCapMenuItem.Name = "seqCapMenuItem";
+            this.seqCapMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.seqCapMenuItem.Text = "連続キャプチャ";
+            this.seqCapMenuItem.Click += new System.EventHandler(this.seqCapMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 812);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.webBrowser1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Tools for Paper";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.ToolStripMenuItem singleCapMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem seqCapMenuItem;
+    }
+}
+
---
+++ windows/ToolsForPaper/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ToolsForPaper
+{
+    public partial class Form1 : Form
+    {
+        private ClipBoardWatcher cbw;
+        private bool capEnable;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cbw = new ClipBoardWatcher();
+            cbw.DrawClipBoard += handleClipboard;
+            capEnable = true;
+        }
+
+        private void seqCapMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!seqCapMenuItem.Checked)
+            {
+                singleCapMenuItem.Checked = false;
+                seqCapMenuItem.Checked = true;
+            }
+        }
+
+        private void singleCapMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!singleCapMenuItem.Checked)
+            {
+                seqCapMenuItem.Checked = false;
+                singleCapMenuItem.Checked = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("http://labs.tokcs.com/nr/");
+        }
+
+        private void toolStripSplitButton1_ButtonClick(object sender, EventArgs e)
+        {
+            toggleCaptureEnable();
+        }
+
+        private void toggleCaptureEnable()
+        {
+            if (capEnable)
+            {
+                cbw.DrawClipBoard -= handleClipboard;
+                toolStripSplitButton1.Image = Properties.Resources.diable;
+                toolStripSplitButton1.ToolTipText = "クリップボード監視オフ";
+                capEnable = false;
+            }
+            else
+            {
+                cbw.DrawClipBoard += handleClipboard;
+                toolStripSplitButton1.Image = Properties.Resources.enable;
+                toolStripSplitButton1.ToolTipText = "クリップボード監視中";
+                capEnable = true;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            cbw.Dispose();
+        }
+
+        private void handleClipboard(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                webBrowser1.Navigate("http://labs.tokcs.com/nr/?t=" + Uri.EscapeDataString(Clipboard.GetText()));
+                if (singleCapMenuItem.Checked)
+                {
+                    toggleCaptureEnable();
+                }
+            }
+        }
+    }
+}
